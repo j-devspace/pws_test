97d3fe21058e20a7938775e6d662c44f965e4918
J DevSpace <jjiijijj@gmail.com>
2025-02-06 21:56:14 +0900

    test

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,14 +1,38 @@
 importScripts("https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js");
 
-self.addEventListener("install", function (e) {
-  self.skipWaiting();
+// self.addEventListener("install", function (e) {
+//   self.skipWaiting();
+// });
+
+// self.addEventListener("activate", function (e) {
+//   WriteLog("fcm service worker가 실행되었습니다.");
+//   console.log("fcm service worker가 실행되었습니다.");
+// });
+
+
+self.addEventListener('install', event => {
+  WriteLog('[Service Worker] 설치됨');
+  event.waitUntil(
+      caches.open('pwa-cache-v1').then(cache => {
+          return cache.addAll([
+              '/index.html'
+          ]);
+      })
+  );
 });
 
-self.addEventListener("activate", function (e) {
-  WriteLog("fcm service worker가 실행되었습니다.");
-  console.log("fcm service worker가 실행되었습니다.");
+self.addEventListener('fetch', event => {
+  event.respondWith(
+      caches.match(event.request).then(response => {
+          return response || fetch(event.request);
+      })
+  );
 });
+
+
+
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyBUTB7eB75zqP2ER1xG7WDRldh9cS-rGy8",
