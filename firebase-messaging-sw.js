df3c45071a2bc19862de10249da54919db10e008
J DevSpace <jjiijijj@gmail.com>
2025-02-21 15:48:04 +0900

    change

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,26 @@
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+// import { getMessaging, getToken, onMessage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging.js";
+
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBUTB7eB75zqP2ER1xG7WDRldh9cS-rGy8",
+//     authDomain: "je-test-e14c9.firebaseapp.com",
+//     projectId: "je-test-e14c9",
+//     storageBucket: "je-test-e14c9.firebasestorage.app",
+//     messagingSenderId: "565490563935",
+//     appId: "1:565490563935:web:b73c5948df8f14b45794ea",
+//     measurementId: "G-137ECMGPH2"
+//   };
+  
+//   const app = initializeApp(firebaseConfig);
+//   const messaging = getMessaging(app);
+  
+//   messaging.onBackgroundMessage((payload) => {
+//     const notificationTitle = payload.title;
+//     const notificationOptions = {
+//         body: payload.body
+//         // icon: payload.icon
+//     };
+//     self.registration.showNotification(notificationTitle, notificationOptions);
+//   });
+  
